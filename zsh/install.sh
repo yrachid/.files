5706b2f54f94936cf6dcdbcb4ab7af0ed41311dd
othman853 <anuar.yasser@gmail.com>
2017-06-04 02:38:38 -0300

    Amber color

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,6 +3,7 @@ mkdir -p $HOME/.othman853/.dotfiles
 cp .zshrc_profile $HOME/.othman853/.dotfiles/.zshrc_profile
 cp .zshrc_alias $HOME/.othman853/.dotfiles/.zshrc_alias
 
+rm -f $HOME/.zshrc
 touch $HOME/.zshrc
 
 echo "source $HOME/.othman853/.dotfiles/.zshrc_profile" >> $HOME/.zshrc
