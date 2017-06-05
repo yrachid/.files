9c94029d2c641e0b4e95c9112f5a54f78beddeb1
othman853 <anuar.yasser@gmail.com>
2017-06-05 19:31:40 -0300

    Simpler code on install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,16 +7,14 @@ function fetch_from_remote() {
   curl --silent $REMOTE_PATH/$1  > $LOCAL_PATH/$1
 }
 
+declare -a DOTFILES=(.profile .alias .env)
+
 mkdir -p $LOCAL_PATH
-
-fetch_from_remote .profile 
-fetch_from_remote .alias 
-fetch_from_remote .env 
-
 rm -f $HOME/.zshrc
 touch $HOME/.zshrc
 
-echo "source $LOCAL_PATH/.profile" >> $HOME/.zshrc
-echo "source $LOCAL_PATH/.alias" >> $HOME/.zshrc
-
-source $HOME/.zshrc
+for dotfile in "${DOTFILES[@]}"
+do
+  fetch_from_remote $dotfile
+  echo "source $LOCAL_PATH/$dotfile" >> $HOME/.zshrc
+done
