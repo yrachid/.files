b134e70c3327ff25306298733d5bc482f6b4ee2e
othman853 <anuar.yasser@gmail.com>
2017-06-05 19:02:43 -0300

    Refactoring remote file fetching

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,13 +1,22 @@
-mkdir -p $HOME/.othman853/dotfiles/zsh
+#!/bin/bash
 
-curl https://raw.githubusercontent.com/othman853/dotfiles/master/zsh/.profile > $HOME/.othman853/dotfiles/zsh/.profile
-curl https://raw.githubusercontent.com/othman853/dotfiles/master/zsh/.alias > $HOME/.othman853/dotfiles/zsh/.alias
-curl https://raw.githubusercontent.com/othman853/dotfiles/master/zsh/.env > $HOME/.othman853/dotfiles/zsh/.env
+REMOTE_PATH=https://raw.githubusercontent.com/othman853/dotfiles/master/zsh
+LOCAL_PATH=$HOME/.othman853/dotfiles/zsh
+
+function fetch_from_remote() {
+  curl --silent $REMOTE_PATH/$1  > $LOCAL_PATH/$1
+}
+
+mkdir -p $LOCAL_PATH
+
+fetch_from_remote .profile 
+fetch_from_remote .alias 
+fetch_from_remote .env 
 
 rm -f $HOME/.zshrc
 touch $HOME/.zshrc
 
-echo "source $HOME/.othman853/dotfiles/zsh/.profile" >> $HOME/.zshrc
-echo "source $HOME/.othman853/dotfiles/zsh/.alias" >> $HOME/.zshrc
+echo "source $LOCAL_PATH/.profile" >> $HOME/.zshrc
+echo "source $LOCAL_PATH/.alias" >> $HOME/.zshrc
 
 source $HOME/.zshrc
