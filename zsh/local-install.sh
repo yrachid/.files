b1f2604d87b8aff7bee5e01b29f5ad0c61ed0861
othman853 <anuar.yasser@gmail.com>
2017-06-07 19:13:22 -0300

    Install script that does not fetch from remote

---
+++ zsh/local-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REMOTE_PATH=$PWD
+LOCAL_PATH=$HOME/.othman853/dotfiles/zsh
+Z_PATH=$HOME/.zshrc
+DOTFILES=(.profile .alias .env)
+
+function fetch_from_remote() {
+  cp $REMOTE_PATH/$1 $LOCAL_PATH/$1
+}
+
+function clean_local_wspace() {
+  rm -rf $LOCAL_PATH
+  mkdir -p $LOCAL_PATH
+  rm -f $ZPATH
+}
+
+function create_zshrc() {
+  for dotfile in "${DOTFILES[@]}"
+  do
+    fetch_from_remote $dotfile
+    echo "source $LOCAL_PATH/$dotfile" >> $Z_PATH
+  done
+}
+
+clean_local_wspace
+chmod +x ./git/git-prompt.sh
+cp -R ./git $HOME/.othman853/dotfiles
+create_zshrc
