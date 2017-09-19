853a03e5661fded20e96455e172a92ad31054e93
othman853 <anuar.yasser@gmail.com>
2017-09-18 22:15:03 -0300

    install.sh support install vim only

--- install.sh
+++ install.sh
@@ -2,21 +2,30 @@
 
 SHELL_TYPE=$1
 
+install_vim() {
+  ln -s $HOME/.othman853/vim $HOME/.vim
+  git submodule update --init
+}
+
 case $SHELL_TYPE in
+  vim)
+    install_vim
+    ;;
+
   bash)
     ln -s $HOME/.othman853/bashrc ~/.bashrc
+    install_vim
   ;;
+
   zsh)
     ln -s $HOME/.othman853/zshrc ~/.zshrc
+    install_vim
   ;;
+
   *)
-    echo "Invalid shell type $SHELL_TYPE"
-    echo "Available options are: bash, zsh"
-    echo "Synopsis: ./install.sh [SHELL]"
+    echo "Invalid install type $SHELL_TYPE"
+    echo "Available options are: vim, bash, zsh"
+    echo "Synopsis: ./install.sh [install-type]"
     exit 1
   ;;
 esac
-
-ln -s $HOME/.othman853/vim ~/.vim
-git submodule update --init
-
