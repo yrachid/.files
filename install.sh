087ecfe89d66aa4ef2627f6f5e3b823121e31190
yrachid <anuar.yasser@gmail.com>
2023-11-11 00:51:46 +0000

    install.sh configures vim, nvim, and tmux

--- install.sh
+++ install.sh
@@ -5,15 +5,41 @@ readonly PRIVATE_CONFIG_FILE="$HOME/.private"
 readonly TOOLS_PATH="$HOME/tools"
 readonly WSPACE_PATH="$HOME/wspace"
 
-rm -f "$HOME/.vim"
-ln -s "$DOTFILES_ROOT/vim" "$HOME/.vim"
+create_file_structure() {
+  touch "$PRIVATE_CONFIG_FILE"
 
-touch "$PRIVATE_CONFIG_FILE"
-mkdir -p "$TOOLS_PATH"
-mkdir -p "$TOOLS_PATH/bin"
-mkdir -p "$WSPACE_PATH"
+  mkdir -p "$TOOLS_PATH"
+  mkdir -p "$TOOLS_PATH/bin"
+  mkdir -p "$WSPACE_PATH"
+}
 
-git submodule update --init
+configure_vim() {
+  local NVIM_CONFIG_PATH="$HOME/.config/nvim"
 
-echo "source $DOTFILES_ROOT/bashrc" > ~/.bashrc
-echo "source $DOTFILES_ROOT/zshrc" > ~/.zshrc
+  curl -fLo "~/.vim/autoload/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  rm -f "$HOME/.vim"
+  ln -s "$DOTFILES_ROOT/vim" "$HOME/.vim"
+
+  mkdir -p "$NVIM_CONFIG_PATH"
+  ln -s "$DOTFILES_ROOT/vim/init.vim" "$NVIM_CONFIG_PATH"
+}
+
+configure_tmux() {
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  ln -s "$DOTFILES_ROOT/rc.d/tmux.conf" "$HOME/.tmux.conf"
+}
+
+enable_config() {
+  echo "source $DOTFILES_ROOT/bashrc" > ~/.bashrc
+  echo "source $DOTFILES_ROOT/zshrc" > ~/.zshrc
+}
+
+
+create_file_structure
+configure_vim
+configure_tmux
